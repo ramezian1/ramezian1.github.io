8e83d53650268942208bf0bd505cf7bab62b84e0
ramezian1 <163072227+ramezian1@users.noreply.github.com>
2024-11-12 22:53:40 -0800

    Delete taskmanager directory

--- taskmanager/src/main/java/com/example/taskmanager/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.taskmanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.taskmanager.model.AppUser;  // Make sure the correct path is used
-
-
-public interface UserRepository extends JpaRepository<AppUser, Long> {
-}
